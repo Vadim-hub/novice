909086553d89c27d0af1f164cad9e4e68e9235d1
Vadim-hub <63390618+Vadim-hub@users.noreply.github.com>
2020-04-18 15:57:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ function Books(label, year, id, pages){
     this.pages = pages;
     this.getPages = function(){
         return this.pages;
-    }
-}
+    };
+};
 
 function Book(title, author, amount){
     Books.call(this);
     this.title = title;
     this.author = author;
     this.amount = amount; 
-}
+};
 
 var book1 = new Book('Tales', 'John', '15');
 book1.label = 'Literatura';
@@ -22,10 +22,10 @@ book1.id = '1';
 book1.pages = '110';
 
 var book2 = new Book('New tales', 'Ian', '20');
-book1.label = 'New literatura';
-book1.year = '2020';
-book1.id = '2';
-book1.pages = '125';
+book2.label = 'New literatura';
+book2.year = '2020';
+book2.id = '2';
+book2.pages = '125';
 
 console.log(book1.getPages());
 console.log(book2.getPages());
@@ -37,24 +37,38 @@ function Journal(volume){
     Books.call(this);
     this.volume = volume;
     this.setPages = function (pages){
-        if(pages <= 0 || pages >= 100)
-        throw "Количество страниц";
+        if(pages <= 0 || pages >= 32)
+        throw "Количество страниц не более 32";
         _pages = pages;
-    }
+    };
     this.getPages = function (){
-        return_pages;
-    }
+        return _pages;
+    };
     this.getYear = function(){
         return this.year;
-    }
-}
+    };
+};
 
 var journal1 = new Journal('№ 11');
-this.label = 'Print';
-this.year = '2001';
-this.id = '382';
+journal1.label = 'Print';
+journal1.year = '2001';
+journal1.id = '382';
+journal1.pages = '28';
 
 var journal2 = new Journal('№ 12');
-this.label = 'New print';
-this.year = '2004';
-this.id = '301';
+journal2.label = 'New print';
+journal2.year = '2004';
+journal2.id = '301';
+journal1.pages = '35';
+
+console.log(journal1.volume);
+console.log(journal2.volume);
+
+console.log(journal1.label);
+console.log(journal2.label);
+
+console.log(journal1.getYear());
+console.log(journal2.getYear());
+
+console.log(journal1.getPages());
+console.log(journal2.getPages());
