46b08002f94486da7a6166254d8de107c8b51008
Vadim-hub <63390618+Vadim-hub@users.noreply.github.com>
2020-04-18 09:51:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,6 +15,24 @@ function Book(title, author, amount){
     this.amount = amount; 
 }
 
+var book1 = new Book('Tales', 'John', '15');
+book1.label = 'Literatura';
+book1.year = '2019';
+book1.id = '1';
+book1.pages = '110';
+
+var book2 = new Book('New tales', 'Ian', '20');
+book1.label = 'New literatura';
+book1.year = '2020';
+book1.id = '2';
+book1.pages = '125';
+
+console.log(book1.getPages());
+console.log(book2.getPages());
+
+console.log(book1.year);
+console.log(book2.year);
+
 function Journal(volume){
     Books.call(this);
     this.volume = volume;
