e532725fdf1dca488aabfaf86c61239486e60bb6
Vadim-hub <63390618+Vadim-hub@users.noreply.github.com>
2020-05-08 07:41:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,77 +1,70 @@
-function Books(label, year, id, pages){
-    this.label = label;
-    this.year = year;
-    this.id = id;
-    this.pages = pages;    
-    this.getPages = function(){
-        return this.pages;
-    };
-};
+let name = document.getElementById('name');
+let surname = document.getElementById('surname');
+let secondName = document.getElementById('secondName');
+let age = document.getElementById('age');
+let profAge = document.getElementById('profAge');
+let date = document.getElementById('date');
 
-function Book(title, author, amount){
-    Books.call(this);
-    this.title = title;
-    this.author = author;
-    this.amount = amount; 
-};
 
-var book1 = new Book('Tales', 'John', '15');
-book1.label = 'Literatura';
-book1.year = '2019';
-book1.id = '1';
-book1.pages = '110';
+function someFunction(){
+    let nameNew = document.getElementById('n');
+    nameNew.innerHTML = name.value;
+    let surnameNew = document.getElementById('s');
+    surnameNew.innerHTML = surname.value;
+    let secondNameNew = document.getElementById('sn');
+    secondNameNew.innerHTML = secondName.value;
+    let ageNew = document.getElementById('a');
+    ageNew.innerHTML = age.value;
+    let profAgeNew = document.getElementById('pa');
+    profAgeNew.innerHTML = profAge.value;
+    let dateNew = document.getElementById('d');
+    dateNew.innerHTML = date.value;
+    }
+    document.onclick = someFunction;
+    
 
-var book2 = new Book('New tales', 'Ian', '20');
-book2.label = 'New literatura';
-book2.year = '2020';
-book2.id = '2';
-book2.pages = '125';
 
-console.log(book1.getPages());
-console.log(book2.getPages());
+function myFunction1() {
+    var newLine = document.createElement("tr");
+    newLine = document.getElementById("myTD");
+    /* var textnode = document.getElementById("myTD");
+    newLine.appendChild(textnode); */
+    /* var textnode = document.createTextNode("Water"); */
+    /* newLine.innerHTML = "Дарэчы новы параграф"; */
+    document.getElementById("myTable").appendChild(newLine);
+    }
 
-console.log(book1.year);
-console.log(book2.year);
+    function myFunction2() {
+    var element = document.getElementById("myTable");
+    document.getElementById("myTable").removeChild(element.lastChild);
+  }
+  
 
-function Journal(volume, _pages){
-    Books.call(this);
-    this.volume = volume;
-    this._pages = pages;
-    this.setPages = function (pages){
-        if(pages <= 0 || pages >= 32)
-        throw "Количество страниц не более 32";
-        _pages = pages;
-    };
-    this.getPages = function (){
-        return _pages;
-    };
-    this.getYear = function(){
-        return this.year;
-    };
-};
 
-var journal1 = new Journal('№ 11');
-journal1.label = 'Print';
-journal1.year = '2001';
-journal1.id = '382';
-journal1.pages = '28';
-journal1._pages = '28';
 
-var journal2 = new Journal('№ 12');
-journal2.label = 'New print';
-journal2.year = '2004';
-journal2.id = '301';
-journal2.pages = '35';
-journal2._pages = '35';
 
-console.log(journal1.volume);
-console.log(journal2.volume);
 
-console.log(journal1.label);
-console.log(journal2.label);
 
-console.log(journal1.getYear());
-console.log(journal2.getYear());
 
-console.log(journal1.getPages());
-console.log(journal2.getPages());
+
+/* var ulSpisok = document.getEletById("table");
+
+inputData.addEventListener("keypress", function(keyPressed){
+    if(keyPressed.which === 13){
+    var newLi = document.createElement("li");
+    var newSpan = document.createElement("span");
+        newSpan.innerHTML = "Delete";
+
+        var newTodo = this.value; /* вспомогательная переменная - Получение value из input */
+        /* this.value = ""; /* Очистка поля ввода (пустая строка) */
+
+        /* ulSpisok.appendChild(newLi).append(newSpan, newTodo);
+    }
+});
+
+
+ulSpisok.appendChild(newTr).append(newTr, newTodo);
+    }
+});
+
+document.getElementById('table').appendChild(tr); */ 
