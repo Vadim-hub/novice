2393e9021c4799615fe37b89e161a365f82cfd9b
Vadim-hub <63390618+Vadim-hub@users.noreply.github.com>
2020-05-23 18:25:00 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,52 +9,52 @@ class Employee {
         this.date = date;
     }
      
-get name() {
-    return this.name = document.getElementById('name');
-} 
-set name(value) {
-    let nameNew = document.getElementById('nam');
-    nameNew.innerHTML = name.value;
-}
-get surname() {
-    return this.surname = document.getElementById('surname');
-}
-set surname(value) {
-    let surnameNew = document.getElementById('sur');
-    surnameNew.innerHTML = surname.value;
-}
-get secondName() {
-    return this.secondName = document.getElementById('secondName');
-}
-set secondName(value) {
-    let secondNameNew = document.getElementById('sn');
-    secondNameNew.innerHTML = secondName.value;
-}
-get age() {
-    return this.age = document.getElementById('age');
-}
-set age(value) {
-    let ageNew = document.getElementById('ag');
-    ageNew.innerHTML = age.value;
-}
-get profAge() {
-    return this.profAge = document.getElementById('profAge');
-}
-set profAge(value) {
-    let profAgeNew = document.getElementById('pa');
-    profAgeNew.innerHTML = profAge.value;
-}
-get date() {
-    return this.date = document.getElementById('date');
+    get name() {
+        return this.name = document.getElementById('name');
+    } 
+    set name(value) {
+        let nameNew = document.getElementById('nam');
+        nameNew.innerHTML = name.value;
     }
-set date(value) {
-    let dateNew = document.getElementById('dat');
-    dateNew.innerHTML = date.value;
-}
-}
+    get surname() {
+        return this.surname = document.getElementById('surname');
+    }
+    set surname(value) {
+        let surnameNew = document.getElementById('sur');
+        surnameNew.innerHTML = surname.value;
+    }
+    get secondName() {
+        return this.secondName = document.getElementById('secondName');
+    }
+    set secondName(value) {
+        let secondNameNew = document.getElementById('sn');
+        secondNameNew.innerHTML = secondName.value;
+    }
+    get age() {
+        return this.age = document.getElementById('age');
+    }
+    set age(value) {
+        let ageNew = document.getElementById('ag');
+        ageNew.innerHTML = age.value;
+    }
+    get profAge() {
+        return this.profAge = document.getElementById('profAge');
+    }
+    set profAge(value) {
+        let profAgeNew = document.getElementById('pa');
+        profAgeNew.innerHTML = profAge.value;
+    }
+    get date() {
+        return this.date = document.getElementById('date');
+        }
+    set date(value) {
+        let dateNew = document.getElementById('dat');
+        dateNew.innerHTML = date.value;
+    }
+    }
 
 class Worker extends Employee {
-    constructor(prof, spec) {
+    constructor(name, surname, secondName, age, profAge, date, prof, spec) {
     super(name, surname, secondName, age, profAge, date);
     this.prof = prof;
     this.spec = spec;
@@ -63,35 +63,22 @@ class Worker extends Employee {
         return this.prof = document.getElementById('prof');
     } 
     set prof(value) {
-        let profNew = document.getElementById('pr');
+        let profNew = document.getElementById('pr2');
         profNew.innerHTML = prof.value;
     }
     get spec() {
         return this.spec = document.getElementById('spec');
     }
     set spec(value) {
-        let specNew = document.getElementById('sp');
+        let specNew = document.getElementById('sp2');
         specNew.innerHTML = spec.value;
     }
-    double(){
-        super.name();
-        super.name(value);
-        super.surname();
-        super.surname(value);
-        super.secondName();
-        super.secondName(value);
-        super.age();
-        super.age(value);
-        super.profAge();
-        super.profAge(value);
-        super.date();
-        super.date(value);
-    }    
+      
 }
 
 document.getElementById('btn').addEventListener('click', () =>{
     let employee = new Employee(); 
-    console.log(employee);
+    alert('Вы добавили нового работника');
     var p = document.getElementById("newTD");
     var p_prime = p.cloneNode(true);
     document.getElementById("myTable").appendChild(p_prime);
@@ -105,7 +92,7 @@ document.getElementById('btn').addEventListener('click', () =>{
 
 document.getElementById('btn2').addEventListener('click', () =>{
     let worker = new Worker(); 
-    console.log(worker);
+    alert('Вы добавили нового специалиста');;
     var w = document.getElementById("myTD");
     var w_prime = w.cloneNode(true);
     document.getElementById("newTable").appendChild(w_prime);
@@ -120,25 +107,13 @@ document.getElementById('btn2').addEventListener('click', () =>{
 });
 
 
-/* function myFunction1() {
-    this.name.value = "";
-    this.surname.value = "";
-    this.secondName.value = "";
-    this.age.value = "";
-    this.profAge.value = "";
-    this.date.value = "";
-} */
-
-
     function myFunction2() {
     var element = document.getElementById("myTable");
     document.getElementById("myTable").removeChild(element.lastChild);
   }
-  
 
-    /* function myFunction2() {
-    var newLine = document.createElement("tr");
-    newLine = document.getElementById("myTD");
-    document.getElementById("myTable").appendChild(newLine);
-    } */ 
-
+  function myFunction3() {
+    var element = document.getElementById("newTable");
+    document.getElementById("newTable").removeChild(element.lastChild);
+  }
+      
